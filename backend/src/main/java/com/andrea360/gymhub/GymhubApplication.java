5901a4ce59e9c35e4038eaa74d2a3d72c7f1ec45
Ana Mićić <148842149+aanmii@users.noreply.github.com>
2026-01-05 22:29:00 +0100

    Setup backend Docker and Docker Compose with Postgres

---
+++ backend/src/main/java/com/andrea360/gymhub/GymhubApplication.java
@@ -0,0 +1,13 @@
+package com.andrea360.gymhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymhubApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/andrea360/gymhub/GymhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrea360.gymhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
